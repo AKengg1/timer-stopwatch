42984c16dea0fcb572f67c7825cf41d53b9302df
AKengg1 <ak72532961@gmail.com>
2026-05-24 17:24:45 +0530

    fix:stopwatch import

--- src/components/stopwatch.jsx
+++ src/components/stopwatch.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import Display from "./Display";
+import Display from "./display";
 
 function Stopwatch() {
   const [elapsed, setElapsed] = useState(0);
